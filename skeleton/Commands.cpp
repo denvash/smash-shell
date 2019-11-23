3c3426b10883397c9e19aefa25d24e5a13e85ae0
denvash <justdenva@gmail.com>
2019-11-23 16:38:47 +0200

    [+] init

---
+++ skeleton/Commands.cpp
@@ -0,0 +1,100 @@
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <sys/wait.h>
+#include <iomanip>
+#include "Commands.h"
+
+using namespace std;
+
+#if 0
+#define FUNC_ENTRY()  \
+  cout << __PRETTY_FUNCTION__ << " --> " << endl;
+
+#define FUNC_EXIT()  \
+  cout << __PRETTY_FUNCTION__ << " <-- " << endl;
+#else
+#define FUNC_ENTRY()
+#define FUNC_EXIT()
+#endif
+
+int _parseCommandLine(const char* cmd_line, char** args) {
+  FUNC_ENTRY()
+  stringstream check1(cmd_line);
+  string intermediate;
+  int i = 0;
+  while(getline(check1, intermediate, ' '))
+  {
+    args[i] = (char*)malloc(intermediate.length()+1);
+    memset(args[i], 0, intermediate.length()+1);
+    strcpy(args[i], intermediate.c_str());
+    args[++i] = NULL;
+  }
+  return i;
+
+  FUNC_EXIT()
+}
+
+bool _isBackgroundComamnd(const char* cmd_line) {
+  const string whitespace = " \t\n";
+  const string str(cmd_line);
+  return str[str.find_last_not_of(whitespace)] == '&';
+}
+
+void _removeBackgroundSign(char* cmd_line) {
+  const string whitespace = " \t\n";
+  const string str(cmd_line);
+  // find last character other than spaces
+  size_t idx = str.find_last_not_of(whitespace);
+  // if all characters are spaces then return
+  if (idx == string::npos) {
+    return;
+  }
+  // if the command line does not end with & then return
+  if (cmd_line[idx] != '&') {
+    return;
+  }
+  // replace the & (background sign) with space and then remove all tailing spaces.
+  cmd_line[idx] = ' ';
+  // truncate the command line string up to the last non-space character
+  cmd_line[str.find_last_not_of(whitespace, idx-1) + 1] = 0;
+}
+
+// TODO: Add your implementation for classes in Commands.h 
+
+SmallShell::SmallShell() {
+// TODO: add your implementation
+}
+
+SmallShell::~SmallShell() {
+// TODO: add your implementation
+}
+
+/**
+* Creates and returns a pointer to Command class which matches the given command line (cmd_line)
+*/
+Command * SmallShell::CreateCommand(const char* cmd_line) {
+	// For example:
+/*
+  string cmd_s = string(cmd_line);
+  if (cmd_s.find("pwd") == 0) {
+    return new GetCurrDirCommand(cmd_line);
+  }
+  else if ...
+  .....
+  else {
+    return new ExternalCommand(cmd_line);
+  }
+  */
+  return nullptr;
+}
+
+void SmallShell::executeCommand(const char *cmd_line) {
+  // TODO: Add your implementation here
+  // for example:
+  // Command* cmd = CreateCommand(cmd_line);
+  // cmd->execute();
+  // Please note that you must fork smash process for some commands (e.g., external commands....)
+}
---
+++ skeleton/signals.h
@@ -0,0 +1,7 @@
+#ifndef SMASH__SIGNALS_H_
+#define SMASH__SIGNALS_H_
+
+void ctrlZHandler(int sig_num);
+void ctrlCHandler(int sig_num);
+
+#endif //SMASH__SIGNALS_H_
